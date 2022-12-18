304c886e96f6a4b26e69baa742de0ee6a3a5b169
Alan Oliveira do Nascimento <alan_nascimento@unigranrio.br>
2022-12-18 17:51:59 -0300

    Novo Update Tkinter 02
    Novo Update Tkinter 02

--- Tkinter/Tkinter 02 - Adicionando um Label e Configurações.py
+++ Tkinter/Tkinter 02 - Adicionando um Label e Configurações.py
@@ -10,7 +10,7 @@ root = Tk()
 #No caso abaixo "750" corresponde à largura e "250" à altura.
 root.geometry("750x250")
 
-#Criação de um Widget de Label(texto). Pode ter qualquer nome do lugar de "texto"
+#Criação de um Widget de Label(texto). Pode ter qualquer nome no lugar de "texto"
 #Dentro de "Label" vai a referência à qual janela ele aparecerá: root.
 #O texto que será mostrado na janela: "Esse é um Widget de Label!"
 #A fonte e tamanho da fonte do texto que será exibido: Fonte: Arial, Tamanho: 20
