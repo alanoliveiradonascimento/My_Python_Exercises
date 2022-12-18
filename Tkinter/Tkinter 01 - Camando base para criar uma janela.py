9d85bc7d19565bf56046ee52def5699969803663
Alan Oliveira do Nascimento <alan_nascimento@unigranrio.br>
2022-12-18 17:07:11 -0300

    Tkinter execises 01 and 02
    Tkinter execises 01 and 02

---
+++ Tkinter/Tkinter 01 - Camando base para criar uma janela.py
@@ -0,0 +1,10 @@
+#O mínimo necessário para criar uma janela com Tkinter
+
+#Importando o módulo Tkinter
+from tkinter import *
+
+#Criação de uma instância de frame ou janela do Tkinter
+root = Tk()
+
+#Loop que mantém a janela aberta. Sem isso a janela se abre e fecha antes de ser mostrada na tela.
+root.mainloop()
---
+++ Tkinter/Tkinter 02 - Adicionando um Label e Configurações.py
@@ -0,0 +1,22 @@
+#O mínimo necessário para criar uma janela com Tkinter
+
+#Importando o módulo Tkinter
+from tkinter import *
+
+#Criação de uma instância de frame ou janela do Tkinter. Pode ter qualquer nome do lugar de "root"
+root = Tk()
+
+#Configuração de "geometria", tamanho do frame ou janela do Tkinter.
+#No caso abaixo "750" corresponde à largura e "250" à altura.
+root.geometry("750x250")
+
+#Criação de um Widget de Label(texto). Pode ter qualquer nome do lugar de "texto"
+#Dentro de "Label" vai a referência à qual janela ele aparecerá: root.
+#O texto que será mostrado na janela: "Esse é um Widget de Label!"
+#A fonte e tamanho da fonte do texto que será exibido: Fonte: Arial, Tamanho: 20
+texto = Label(root, text="Esse é um Widget de Label!", font='Arial 20')
+#Pack é o método que irá inserir o Lable texto na tela.
+texto.pack()
+
+#Loop que mantém a janela aberta. Sem isso a janela se abre e fecha antes de ser mostrada na tela.
+root.mainloop()
