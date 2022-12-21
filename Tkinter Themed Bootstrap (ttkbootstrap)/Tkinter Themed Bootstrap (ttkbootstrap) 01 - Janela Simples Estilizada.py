9f7719233b23cfe84167b7eddd7eb91292a3e0ff
Alan Oliveira do Nascimento <alan_nascimento@unigranrio.br>
2022-12-20 21:52:23 -0300

    Bootstrap 01 - update
    Bootstrap 01 - update

--- Tkinter Themed Bootstrap (ttkbootstrap)/Tkinter Themed Bootstrap (ttkbootstrap) 01 - Janela Simples Estilizada.py
+++ Tkinter Themed Bootstrap (ttkbootstrap)/Tkinter Themed Bootstrap (ttkbootstrap) 01 - Janela Simples Estilizada.py
@@ -11,10 +11,18 @@ import ttkbootstrap as ttkb
 
 root = ttkb.Window(themename="darkly")
 
+# Título da Janela
 root.title("TTK BootStrap!")
+
+# Ícone no topo da Janela
 root.iconbitmap('python.ico')
+
+# Tamanho da Janela
 root.geometry('500x300')
 
+#Criando um contador e uma classe onde:
+# Se o contador for par (counter % 2 == 0) o label será alterado para "Hello World!"
+# Se não (se for ímpar), o label será alterado para "Goobye World!"
 counter = 0
 def changer():
     global counter
@@ -34,5 +42,18 @@ my_label.pack(pady=50)
 # Se no lugar de outline colocarmos o comando "link" o botão fica com a aparência de um link.
 my_button = ttkb.Button(text="Botão", bootstyle="success, outline", command=changer)
 my_button.pack()
+
+# Tamanho da Janela
+largura = 490
+altura = 250
+
+# Resolução do Sistema (altura e largura do monitor)
+largura_monitor = root.winfo_screenwidth()
+altura_monitor = root.winfo_screenheight()
+
+# Posição da Janela em relação ao monitor em uso.
+posx = largura_monitor / 2 - largura / 2
+posy = altura_monitor / 2 - altura / 2
+root.geometry('%dx%d+%d+%d' % (largura, altura, posx, posy))
 
 root.mainloop()
