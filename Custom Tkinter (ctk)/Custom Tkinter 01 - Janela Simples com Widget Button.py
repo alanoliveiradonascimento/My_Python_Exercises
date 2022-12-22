496d4d82e8fe70a218c60d0792c59eac071f3fea
Alan Oliveira do Nascimento <alan_nascimento@unigranrio.br>
2022-12-22 01:14:57 -0300

    Custom Tkinter 01 - update 01
    Custom Tkinter 01 - update 01

--- Custom Tkinter (ctk)/Custom Tkinter 01 - Janela Simples com Widget Button.py
+++ Custom Tkinter (ctk)/Custom Tkinter 01 - Janela Simples com Widget Button.py
@@ -1,16 +1,21 @@
+# Importando Tkinter
 from tkinter import *
+# Importando customtkinter e atribuindo um "apelido" ctk
 import customtkinter as ctk
 
+# Configuração de janela do Tkinter: Quem é a janela, Título da janela, dimensões e cor de fundo.
 root = Tk()
 root.title('Teste CTK')
 root.geometry("450x250")
 root.configure(background="light grey")
 
 
+# Função para mostrar no terminal se o botão foi clicado
 def clicou():
     print("Clicou, né!?")
 
 
+# Widget Button do CTK
 botao = ctk.CTkButton(text="Clica aí!", command=clicou)
 botao.pack(anchor=CENTER, pady=100)
 
